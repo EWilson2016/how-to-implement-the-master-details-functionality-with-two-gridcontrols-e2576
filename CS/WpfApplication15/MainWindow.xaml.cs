b8edf1b673692da20d0b5934ab3fd74d5dc51043
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication15/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace WpfApplication15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public DataSet CreateData()
+        {
+            DataTable mdt = new DataTable("Company");
+            mdt.Columns.Add(new DataColumn("Name", typeof(string)));
+            mdt.Columns.Add(new DataColumn("ID", typeof(int)));
+            mdt.Rows.Add("Ford", 4);
+            mdt.Rows.Add("Nissan", 5);
+            mdt.Rows.Add("Mazda", 6);
+
+            DataTable ddt = new DataTable("Models");
+            ddt.Columns.Add(new DataColumn("Name", typeof(string)));
+            ddt.Columns.Add(new DataColumn("MaxSpeed", typeof(int)));
+            ddt.Columns.Add(new DataColumn("CompanyName", typeof(string)));
+            ddt.Rows.Add("FordFocus", 400, "Ford");
+            ddt.Rows.Add("FordST", 400, "Ford");
+            ddt.Rows.Add("Note", 1000, "Nissan");
+            ddt.Rows.Add("Mazda3", 1000, "Mazda");
+
+            ds = new DataSet("CM");
+            ds.Tables.Add(mdt);
+            ds.Tables.Add(ddt);
+            DataRelation dr = new DataRelation("CompanyModels", mdt.Columns["Name"], ddt.Columns["CompanyName"]);
+            ds.Relations.Add(dr);
+
+            return ds;
+         }
+
+        DataSet ds;
+        bool IsLoaded = false;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ds = this.CreateData();
+            gridControl1.ItemsSource = ds.Tables["Company"];
+            IsLoaded = true;
+        }
+
+        private void TableView_FocusedRowChanged(object sender, DevExpress.Xpf.Grid.FocusedRowChangedEventArgs e)
+        {
+            if (IsLoaded)
+            {
+                DataRowView drv = gridControl1.GetRow(gridControl1.View.FocusedRowHandle) as DataRowView;
+                DataView dv = new DataView(ds.Tables["Models"]);
+                dv.RowFilter = String.Format("CompanyName = '{0}'", drv["Name"].ToString());
+                gridControl2.ItemsSource = dv;
+            }
+        }
+    }
+}
---
+++ VB/WpfApplication15/MainWindow.xaml.vb
@@ -0,0 +1,70 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.Data
+
+Namespace WpfApplication15
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Public Function CreateData() As DataSet
+			Dim mdt As New DataTable("Company")
+			mdt.Columns.Add(New DataColumn("Name", GetType(String)))
+			mdt.Columns.Add(New DataColumn("ID", GetType(Integer)))
+			mdt.Rows.Add("Ford", 4)
+			mdt.Rows.Add("Nissan", 5)
+			mdt.Rows.Add("Mazda", 6)
+
+			Dim ddt As New DataTable("Models")
+			ddt.Columns.Add(New DataColumn("Name", GetType(String)))
+			ddt.Columns.Add(New DataColumn("MaxSpeed", GetType(Integer)))
+			ddt.Columns.Add(New DataColumn("CompanyName", GetType(String)))
+			ddt.Rows.Add("FordFocus", 400, "Ford")
+			ddt.Rows.Add("FordST", 400, "Ford")
+			ddt.Rows.Add("Note", 1000, "Nissan")
+			ddt.Rows.Add("Mazda3", 1000, "Mazda")
+
+			ds = New DataSet("CM")
+			ds.Tables.Add(mdt)
+			ds.Tables.Add(ddt)
+			Dim dr As New DataRelation("CompanyModels", mdt.Columns("Name"), ddt.Columns("CompanyName"))
+			ds.Relations.Add(dr)
+
+			Return ds
+		End Function
+
+		Private ds As DataSet
+		Private IsLoaded As Boolean = False
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			ds = Me.CreateData()
+			gridControl1.ItemsSource = ds.Tables("Company")
+			IsLoaded = True
+		End Sub
+
+		Private Sub TableView_FocusedRowChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.FocusedRowChangedEventArgs)
+			If IsLoaded Then
+				Dim drv As DataRowView = TryCast(gridControl1.GetRow(gridControl1.View.FocusedRowHandle), DataRowView)
+				Dim dv As New DataView(ds.Tables("Models"))
+				dv.RowFilter = String.Format("CompanyName = '{0}'", drv("Name").ToString())
+				gridControl2.ItemsSource = dv
+			End If
+		End Sub
+	End Class
+End Namespace
